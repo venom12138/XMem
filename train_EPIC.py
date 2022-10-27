8a9735ff2a221e5d8113c661b4513050d6e4fdeb
venom12138 <1390852413@qq.com>
2022-10-27 21:22:21 +0800

    PC-1027 action classify

--- train_EPIC.py
+++ train_EPIC.py
@@ -123,7 +123,7 @@ def get_EPIC_parser():
     parser.add_argument('--teacher_loss_weight', default=0.1, type=float)
     
     parser.add_argument('--fuser_type', default='cbam', type=str, choices=['cbam','cross_attention'])
-    parser.add_argument('--remove_hands', default=1, type=int, choices=[0,1])
+    parser.add_argument('--remove_hands', default=0, type=int, choices=[0,1])
     parser.add_argument('--classifiy_action', default=0, type=int, choices=[0,1])
     parser.add_argument('--action_num_classes', default=30, type=int)
     
